7bb611ff94515716c8927b585b3023ec2b07826c
Renette Ros <supergirl0894+git@gmail.com>
2024-11-28 09:35:32 +0200

    Day 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -3,10 +3,27 @@ package day1
 import helper.Debug
 
 fun solveA(text: String, debug: Debug = Debug.Disabled): Int {
-    return 0
+
+    return text.fold(0) { acc, char ->
+        when (char) {
+            '(' -> acc + 1
+            ')' -> acc - 1
+            else -> TODO("Invalid char $char")
+        }
+    }
 }
 
 
 fun solveB(text: String, debug: Debug = Debug.Disabled): Int {
-    return 0
+    var acc = 0
+    text.forEachIndexed() { i, char ->
+        acc = when (char) {
+            '(' -> acc + 1
+            ')' -> acc - 1
+            else -> TODO("Invalid char $char")
+        }
+
+        if (acc < 0) return i + 1
+    }
+    return -1
 }
--- src/test/kotlin/day1/Day1KtTest.kt
+++ src/test/kotlin/day1/Day1KtTest.kt
@@ -13,12 +13,11 @@ internal class Day1KtTest {
 
     @Test
     fun sample1() {
-        val text = """
-            |
-        """.trimMargin().trimEnd()
 
-        assertEquals(0, solveA(text, Debug.Enabled))
-        assertEquals(0, solveB(text, Debug.Enabled))
+        assertEquals(0, solveA("(())", Debug.Enabled))
+        assertEquals(0, solveA("()()", Debug.Enabled))
+        assertEquals(1, solveB(")", Debug.Enabled))
+        assertEquals(5, solveB("()())", Debug.Enabled))
     }
 
     @Test
@@ -27,10 +26,10 @@ internal class Day1KtTest {
 //        val text = readDayFile(day, "sample2.in").readText().trimEnd()
 
         val text = """
-            |
+            |232
         """.trimMargin().trimEnd()
 
-        assertEquals(0, solveA(text, Debug.Enabled))
+        assertEquals(232, solveA(text, Debug.Enabled))
         assertEquals(0, solveB(text, Debug.Disabled))
     }
 
@@ -40,10 +39,10 @@ internal class Day1KtTest {
 
         val solveA = solveA(lines)
         println("A: $solveA")
-        assertEquals(0, solveA)
+        assertEquals(232, solveA)
 
         val solveB = solveB(lines)
         println("B: $solveB")
-        assertEquals(0, solveB)
+        assertEquals(1783, solveB)
     }
 }
